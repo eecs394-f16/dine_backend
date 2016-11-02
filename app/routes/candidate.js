596979de1eaef6182de9736dd7d47c14a1ac3a6d
Devon D'Apuzzo <djdapz@aol.com>
2016-11-02 11:52:17 -0500

    candidates now returns boolean of if they liked the user

--- app/routes/candidate.js
+++ app/routes/candidate.js
@@ -4,8 +4,16 @@ var getAllCandidates = function(req,res){
   var userId = req.query.userId;
 
   //TODO - figure out how to get x amount at a time
-  var sqlString = "SELECT * FROM users where not id = "+userId+";"
-
+  var sqlString = "SELECT u.*, ulk.likes"
+                + " from users u"
+                + " LEFT OUTER JOIN"
+                + "   ("
+                + "       SELECT *"
+                + "       from user_likes_candidate ulk"
+                + "       where ulk.candidate = " + userId
+                + "   ) as ulk"
+                + " on u.id = ulk.user_id"
+                +" where not u.id = "+userId+ ";";
   db.interactWithDatabase(sqlString,
     //on success
     function(data, error){
