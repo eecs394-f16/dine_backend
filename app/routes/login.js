c2ef6bc561c8a768fcae8b834cfb758d7f987417
Devon D'Apuzzo <djdapz@aol.com>
2016-11-09 17:40:54 -0600

    unauthorized error if password incorrect

--- app/routes/login.js
+++ app/routes/login.js
@@ -11,6 +11,7 @@ var login = function(req, res) {
         //on success
         function(data, error) {
             if (error) {
+                res.status(401);
                 res.json({type: "error", message:"This user - password combination was not found"});
             } else {
                 userService.getUser(data[0].user_id,
