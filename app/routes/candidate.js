f2641ac556c1b30cdb45642ea43ca56e25ceaee0
cory17 <corycolbert2017@u.northwestern.edu>
2016-11-26 15:14:08 -0600

    Added location matching, i_like, and removal of mutual matches to candidates

--- app/routes/candidate.js
+++ app/routes/candidate.js
@@ -7,47 +7,66 @@ db = require('../../config/database.js');
  */
 var getAllCandidates = function(req,res){
     var userId = req.query.userId;
+    var miles = req.query.miles ? req.query.miles : 20
+    
+    var sqlString = 'WITH likes AS (' +
+	' SELECT u.candidate AS id, true AS i_like ' +
+	' FROM user_likes_candidate u' +
+	' WHERE u.user_id = ' + userId +
+    ' ),' +
+	' not_liked AS (' +
+	    ' SELECT users.id AS id, false AS i_like' +
+	    ' FROM users' +
+	    ' WHERE users.id' +
+            ' NOT IN (' +
+		' SELECT id FROM likes' +
+            ' )' +
+            ' AND users.id != ' + userId +
+	' ),' +
+	' likes_excluding_matches AS (' +
+	    ' SELECT * FROM likes' +
+	    ' EXCEPT' +
+	    ' SELECT u.user_id AS id, true AS i_like' +
+	    ' FROM user_likes_candidate u' +
+	    ' WHERE u.candidate = ' + userId +
+	' ),' +
+	' candidates AS (' +
+	    ' SELECT * FROM likes_excluding_matches' +
+	    ' UNION' +
+	    ' SELECT * FROM not_liked' +
+	' ),' +
+	' nearby_candidates AS (' +
+	    ' SELECT *' +
+	    ' FROM user_location' +
+            ' JOIN candidates ON' +
+            ' user_location.user_id = candidates.id' +
+	    ' ,' +
+            ' LATERAL (' +
+		' SELECT *' +
+     		' FROM user_location' +
+		' WHERE user_location.user_id = ' + userId +
+            ' ) coordinates,' +
+            ' LATERAL (' +
+		' SELECT ST_DistanceSphere(' +
+                    ' ST_MakePoint(' +
+			' coordinates.longitude,' +
+			' coordinates.lattitude' +
+	            ' ),' +
+	            ' ST_MakePoint(' +
+			' user_location.longitude,' +
+			' user_location.lattitude' +
+                    ' )' +
+		' ) AS distance_km' +
+            ' ) distance_km' +
+	    ' WHERE distance_km <= (' + miles + ' * 1609.344) AND' +
+            ' user_location.user_id != ' + userId +
+	' )' +
+    ' SELECT users.*, nearby_candidates.i_like' +
+    ' FROM nearby_candidates' +
+    ' JOIN users ON' +
+    ' users.id = nearby_candidates.id';
 
-    // var users_cte = "SELECT DISTINCT ON (candidates.user_id) *, candidates.user_id as id"
-    // + " FROM user_likes_candidate candidates,"
-	// + " LATERAL ("
-	//     + " SELECT CASE WHEN EXISTS("
-	// 	+ " SELECT " + userId + ", candidate"
-	// 	  + " FROM user_likes_candidate"
-	// 	+ " WHERE user_id = candidates.user_id AND"
-	// 	+ " candidate = " + userId
-	//     + " ) THEN true ELSE false END AS i_like"
-	// 	  + " ) t, LATERAL ( SELECT * FROM users ) info"
-    // + " WHERE candidates.user_id != " + userId;
-    //
-    // var miles = 200;
-    //
-    // var sqlString =
-	// "WITH users_cte AS ( " + users_cte + " )"
-     //     + " SELECT users_cte.*"
-     //     + " FROM user_location"
-     //         + " JOIN users_cte ON  user_location.user_id = users_cte.user_id,"
-     //         + " LATERAL ("
-     //            + " SELECT *"
-	//           + " FROM user_location"
-	//           + " WHERE user_location.user_id = " + userId
-	//       + " ) coordinates,"
-	//       + " LATERAL ("
-     //              + " SELECT ST_DistanceSphere("
-	// 	      + " ST_MakePoint("
-	//                   + " coordinates.longitude,"
-	//                   + " coordinates.lattitude"
-	// 	      + " ),"
-	// 	      + " ST_MakePoint("
-	//                   + " user_location.longitude,"
-	//                   + " user_location.lattitude"
-	// 	      + " )"
-     //              + " ) AS distance_km"
-     //          + " ) distance_km"
-     //    + " WHERE distance_km <= (" + miles + "* 1609.344) AND"
-     //        + " user_location.user_id != " + userId;
-
-    var sqlString ="SELECT DISTINCT u_and_ulk.*, ulk2.likes as i_like FROM"
+   /* var sqlString ="SELECT DISTINCT u_and_ulk.*, ulk2.likes as i_like FROM"
 					+" ( SELECT u.*, ulk.likes FROM users u"
 					+" LEFT OUTER JOIN"
 					+" (SELECT * FROM user_likes_candidate ulk WHERE ulk.candidate = "+userId+" ) AS ulk"
@@ -56,7 +75,7 @@ var getAllCandidates = function(req,res){
 					+" LEFT OUTER JOIN"
 					+" (SELECT * from user_likes_candidate ulk  where ulk.user_id = "+userId+") as ulk2"
 					+" on u_and_ulk.id = ulk2.candidate"
-					+" where ulk2.likes is null or u_and_ulk.likes is null ;";
+					+" where ulk2.likes is null or u_and_ulk.likes is null ;";*/
 
 
 
